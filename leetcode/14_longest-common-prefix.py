061b5bf7a2252976e92ae79d5b550c561b0646d1
kazoo <kaz206@gmail.com>
2022-07-28 11:25:25 +0900

    refine #14

--- leetcode/14_longest-common-prefix.py
+++ leetcode/14_longest-common-prefix.py
@@ -4,7 +4,7 @@ from re import S
 from typing import List
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix2(self, strs: List[str]) -> str:
         prefix = strs[0]
         
         for i in range(1, len(strs)):
@@ -12,12 +12,25 @@ class Solution:
                 break
             j = 0
             while j <= len(strs[i]) and j < len(prefix):
-                print(i, j)
                 if j == len(strs[i]) or strs[i][j] != prefix[j]:
                     prefix = prefix[:j]
                     break
                 j += 1
         return prefix
+
+    # 先に最短をprefixにするとスマート & pythonic!
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # 組み込み関数をkeyに指定できる。
+        # max(nums, key=abs)
+        # sorted(strs, key=len)
+        # といったものも
+        shortest = min(strs,key=len)
+
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest
 
 sl = Solution()
 strs = ["dog","racecar","car"]
