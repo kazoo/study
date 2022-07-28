e4094e445d2949dbbdf8568b3f8684c40b0adfd2
kazoo <kaz206@gmail.com>
2022-07-28 12:56:25 +0900

    add reference solution on #19

--- leetcode/19_remove-nth-linked-list.py
+++ leetcode/19_remove-nth-linked-list.py
@@ -9,7 +9,42 @@ class ListNode:
         self.next = next
         
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+    # 再帰で後ろからインデックスしてi>nを全部ずらす
+    def removeNthFromEnd3(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        def index(node):
+            if not node:
+                return 0
+            i = index(node.next) + 1
+            if i > n:
+                node.next.val = node.val
+            return i
+        index(head)
+        return head.next
+
+    # ???
+    def removeNthFromEnd2(self, head, n):
+        def remove(head):
+            if not head:
+                return 0, head
+            i, head.next = remove(head.next)
+            return i+1, (head, head.next)[i+1 == n]
+        return remove(head)[1]
+
+    # fast and slow pointer
+    def removeNthFromEnd(self, head, n):
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
+
+    def removeNthFromEnd4(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         current = head
         cnt = 0
         lp = []
