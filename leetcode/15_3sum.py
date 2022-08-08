701eb1cbfb2ca47c720c59449c882f3099dcb4ef
kazoo <kaz206@gmail.com>
2022-08-08 18:44:46 +0900

    add submission #15

--- leetcode/15_3sum.py
+++ leetcode/15_3sum.py
@@ -3,19 +3,67 @@
 from typing import List
 
 class Solution:
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+
+        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0:
+                n.append(num)
+            else:
+                z.append(num)
+
+        #2. Create a separate set for negatives and positives for O(1) look-up times
+        N, P = set(n), set(p)
+
+        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
+        #   i.e. (-3, 0, 3) = 0
+        if z:
+            for num in P:
+                if -1*num in N:
+                    res.add((-1*num, 0, num))
+
+        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
+        if len(z) >= 3:
+            res.add((0,0,0))
+
+        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
+        #   exists in the positive number set
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                target = -1*(n[i]+n[j])
+                if target in P:
+                    res.add(tuple(sorted([n[i],n[j],target])))
+
+        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
+        #   exists in the negative number set
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                target = -1*(p[i]+p[j])
+                if target in N:
+                    res.add(tuple(sorted([p[i],p[j],target])))
+
+        return res
+
+    # TLE...?
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
 
         def combinations(nums, r):
             if r == 1:
                 return [[v] for v in nums]
-            result = set()
+            result = []
             for n, val in enumerate(nums):
                 rest = combinations(nums[n+1:], r-1)
 
                 for _ in rest:
                     comb = sorted([val] + _)
-#                    if comb not in result:
-                    result.add(tuple(comb))
+                    if comb not in result:
+                        result.append(comb)
+
                     # if comb not in result and sum(comb) == 0 and len(comb) == 3:
                     #     print("{}, sum:{}".format(comb, sum(comb)))
 #                    if sum(comb) == 0: print(comb, sum(comb))
@@ -29,8 +77,10 @@ class Solution:
             return result
 
 #        return list(combinations(nums, 3))
-#        return list(filter(lambda x:sum(x) == 0, combinations(nums, 3)))
-        return list(map(lambda x:list(x), filter(lambda x:sum(x) == 0, list(combinations(nums, 3)))))
+        return list(filter(lambda x:sum(x) == 0, combinations(nums, 3)))
+#        return list(map(lambda x:list(x), filter(lambda x:sum(x) == 0, list(combinations(nums, 3)))))
+#        combinations(nums, 3)
+#        return []
 
 
 
