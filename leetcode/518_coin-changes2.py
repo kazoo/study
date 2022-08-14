0296d3abb327f59de2cbc40d02ad1b8f087c0b2b
kazoo <kaz206@gmail.com>
2022-08-14 11:00:52 +0900

    add some comments

--- leetcode/518_coin-changes2.py
+++ leetcode/518_coin-changes2.py
@@ -5,7 +5,35 @@ from typing import List
 
 
 class Solution:
+
+    # dp[i][j] : i番目までの種類のコインで amount:j を作成できる組み合わせの数
+    # 状態遷移は
+    #
+    # 1. i番目のコインを使わない → i-1番目までで j を作る → dp[i-1][j]
+    # 2. i番目のコインを使う → 同じ種類のコインを何個使っても良いので、
+    #    (i番目も使って）何通りで `j - coins[i-1]` を作れるかを調べる
+    #    → すなわち、dp[i][j-coins[i-1]]
+    #
+    # dp[i][0] = 1 で初期化
+    #
+    # ここまでわかれば
     def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0 for _ in range(amount + 1)]] * (len(coins) + 1)
+        dp[0][0] = 1
+
+        for i in range(1, len(coins)):
+            dp[i][0] = 1
+            for j in range(1, amount):
+                if j >= coins[i-1]:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[len(coins)][amount]
+
+    # さらに
+    # dp[i][j] は、dp[i-1][j] と dp[i][j-coins[i]] のみに依存しているので、
+    # 1次元配列に最適化できる
+    def change2(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
         dp[0] = 1
         for i in coins:
@@ -15,7 +43,7 @@ class Solution:
         return dp[amount]
 
     # WA...
-    def change2(self, amount: int, coins: List[int]) -> int:
+    def change3(self, amount: int, coins: List[int]) -> int:
         ans = 0
         q = deque(coins)
 
