6622d4dde759e8f4a680dda4236ce55bda99f8f1
kazoo <kaz206@gmail.com>
2022-08-15 19:29:13 +0900

    fix #70

--- leetcode/70_climbing-stairs.py
+++ leetcode/70_climbing-stairs.py
@@ -1,11 +1,39 @@
 # https://leetcode.com/problems/climbing-stairs/
 
+# 一応再起処理の上限を増やしたい時は
+# import sys
+# sys.setrecursionlimit(2000)
+# で、できる。デフォルトは1000
 class Solution:
+
     def climbStairs(self, n: int) -> int:
-        if n == 1 or n == 2:
+        if n <= 2:
             return n
 
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        two_steps_before = 1
+        one_step_before = 2
+        ans = 0
+
+        for i in range(2, n):
+            ans = two_steps_before + one_step_before
+            two_steps_before = one_step_before
+            one_step_before = ans
+        return ans
+
+    # leetcode では maximum recursion depth exceeded
+    def climbStairs2(self, n: int) -> int:
+        memo = {}
+
+        def step(n):
+            if n <= 2:
+                return n
+            if n in memo:
+                return memo[n]
+
+            memo[n] = step(n-1) + step(n-2)
+
+            return memo[n]
+        return step(n)
 
 
 sl = Solution()
