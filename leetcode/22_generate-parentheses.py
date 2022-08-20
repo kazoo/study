d7b3f0be53dff3d695ff3da28722d86e8a364054
kazoo <kaz206@gmail.com>
2022-08-21 00:13:46 +0900

    add submission #22

---
+++ leetcode/22_generate-parentheses.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/generate-parentheses/
+
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+
+        def bfs(pairs, stack, rest):
+            print(pairs, stack, rest)
+            if rest <= 0:
+                if stack == 0:
+                    ans.append(pairs)
+                return
+
+            if stack > 0:
+                bfs(pairs + ")", stack - 1, rest - 1)
+
+            if rest > 0:
+                bfs(pairs + "(", stack + 1, rest - 1)
+
+        bfs("(", 1, n*2-1)
+        return ans
+
+sl = Solution()
+print(sl.generateParenthesis(3))
