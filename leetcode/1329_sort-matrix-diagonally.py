2a61e3c981e6426b446091a1592754c3859d22ac
kazoo <kaz206@gmail.com>
2022-08-28 23:57:37 +0900

    update #1329

--- leetcode/1329_sort-matrix-diagonally.py
+++ leetcode/1329_sort-matrix-diagonally.py
@@ -1,11 +1,27 @@
 # https://leetcode.com/problems/sort-the-matrix-diagonally/
 
+from collections import defaultdict
 from pprint import pprint
 from typing import List
 
 
 class Solution:
+    # from discussions
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        d = defaultdict(list)
+        for i in range(m):
+            for j in range(n):
+                d[i - j].append(mat[i][j])
+        for k in d:
+            d[k].sort(reverse=True)
+        for i in range(m):
+            for j in range(n):
+                mat[i][j] = d[i - j].pop()
+        return mat
+
+    # my solution
+    def diagonalSort2(self, mat: List[List[int]]) -> List[List[int]]:
         m = len(mat)
         n = len(mat[0])
 
