de43a8f0017444c8a13eea97f116c30cf4c87bf0
kazoo <kaz206@gmail.com>
2022-09-11 10:52:48 +0900

    fix #1996

--- leetcode/1996_number-of-weak.py
+++ leetcode/1996_number-of-weak.py
@@ -1,9 +1,27 @@
 # https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
 
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
+    #
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        d = defaultdict(list)
+        for atk, dfn in properties:
+            d[atk].append(dfn)
+
+        weak, max_dfn = 0, -1
+        for k in sorted(d.keys(), reverse=True):
+            for dfn in d[k]:
+                if dfn < max_dfn:
+                    weak += 1
+
+            max_dfn = max(max_dfn, max(d[k]))
+
+        return weak
+
+    # TLE
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         max_weak = 0
         weakers = []
