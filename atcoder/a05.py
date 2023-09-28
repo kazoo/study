70393c246261e534f33e739dab69a50b0363f8b2
kazoo <kaz206@gmail.com>
2023-09-29 07:47:51 +0900

    add ac_A05

---
+++ atcoder/a05.py
@@ -0,0 +1,23 @@
+n,m = map(int, input().split())
+
+if 3 * n < m:
+    print(0)
+    exit()
+
+ans = 0
+for i in range(1, n+1):
+    if i > m - 2:
+        break
+    for j in range(1, n+1):
+        if i + j > m - 1:
+            break
+        if m - (i + j) <= n:
+            ans += 1
+        # for k in range(1, n+1):
+        #     print(i, j, k)
+        #     if i + j + k > m:
+        #         break
+        #     if i + j + k == m:
+        #         print(i, j, k)
+        #         ans += 1
+print(ans)
